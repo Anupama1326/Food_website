2ec69e695fd7184ea1d25d0540b68747a2e9904d
Anupama1326 <98605703+Anupama1326@users.noreply.github.com>
2022-01-28 21:50:43 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:"prev-button",
+        nextArrow:"next-button",
+        
+
+    });
+})
---
+++ js/app2.js
@@ -0,0 +1,29 @@
+$(document).ready(function()
+{
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:"prev-button",
+        nextArrow:"next-button",
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+
+        ]
+
+    });
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled');
+    })
+})
